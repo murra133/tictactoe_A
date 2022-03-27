90107b77d0108c4903f7234293a8817c61212c0b
aakorede2 <akorede_a@yahoo.com>
2022-03-27 00:47:44 -0400

    Added Tasks

--- tictactoe.py
+++ tictactoe.py
@@ -9,14 +9,30 @@ def display(arr):
         print(bottom)
 
 #Add Change
+#   y0,y1,y2
+#x0[- ,- ,-]
+#x1[- ,- ,-]
+#x2[- ,- ,-]
 l=0
 k="O"
 arr = [["-","-","-"],["-","-","-"],["-","-","-"]]
 display(arr)
 while(l < 3):
+    
     x = int(input("Please input the X Value of your attack:"))
     y = int(input("Please input the Y Value of your attack:"))
     arr[x][y] = k
     display(arr)
     l=l+1
      
+## Win Condition (A)
+
+##Draw Conditions ()
+
+#Modes 2 Players or 1 Player (A)
+
+#Double Win ()
+
+#Listener to not place item in already filled slot Ex. If X exists O cannot be placed (A)
+
+# Listener to swap players ()
